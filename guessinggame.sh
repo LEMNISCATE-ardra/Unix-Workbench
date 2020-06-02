fe2e6854fe856873ebf2ef9ee728d69f31368d55
LEMNISCATE-ardra <65713868+LEMNISCATE-ardra@users.noreply.github.com>
2020-06-02 20:33:57 +0530

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,25 @@
+
+function game
+{
+  echo "Please enter your guess(no of files): "
+  read $input
+  echo $input
+  file = $(pwd | ls | wc -l)
+
+  while [[ $input -ne $file ]]
+  do
+	
+  	if [[ $input -lt $file ]]
+  	then
+      		echo "Wrong!!,Try a bigger no"
+    	elif [[ $input -gt $file ]]
+    	then
+      		echo "Wrong!!,Try a smaller no"
+    	elif [[ $input -eq $file ]]
+    	then 
+		echo "Correct answer, Thank you for participating" 
+   	fi
+  done
+}
+
+game
---
+++ makefile
@@ -0,0 +1,7 @@
+README.md: guessinggame.sh
+                                 
+	echo ' Guessing Game  > README.md
+	echo 'Time: $( date +%Y-%m-%d:%H:%M:%S)\n' >> README.md
+	echo 'NO. Of line'>>README.md
+	cat guessinggame.sh|wc-1 >> README.md
+
