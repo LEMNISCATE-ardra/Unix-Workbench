d10ea7c8806adfa4a99bef9461dd821e722b68a9
LEMNISCATE-ardra <65713868+LEMNISCATE-ardra@users.noreply.github.com>
2020-06-03 18:04:03 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,27 +1,30 @@
 
+file=$(ls -1| wc -l)
+echo "Please enter your guess(no of files): "
 function game
-{
-  echo "Please enter your guess(no of files): "
-  read $input
-  
-  file = $(ls -1| wc -l)
-
-  while [[ $input -ne $file ]]
-  do
-	
-  	if [[ $input -lt $file ]]
-  	then
-      		echo "Wrong!!,Try a bigger no"
-		
-    	elif [[ $input -gt $file ]]
-    	then
-      		echo "Wrong!!,Try a smaller no"
-		
-    	elif [[ $input -eq $file ]]
-    	then 
-		echo "Correct answer, Thank you for participating" 
-   	fi
-  done
-}
+{  
+  read input
+ }
+ 
 
 game
+if [[ $input -eq $file ]]
+then 
+     echo "Correct answer, Thank you for participating" 
+else
+    while [[ $input -ne $file ]]
+    do
+       if [[ $input -lt $file ]]
+       then
+      		echo "Wrong!!,Try a bigger no"
+       elif [[ $input -gt $file ]]
+       then
+      		echo "Wrong!!,Try a smaller no"
+		
+       game
+       fi
+     done
+fi
+   	
+    
+
